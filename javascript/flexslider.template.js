d34de3f7c3e79a7f683ae1654f8f084ea6835194
Anselm Christophersen <ac@anselm.dk>
2013-10-30 15:29:26 +0100

    fixed js init issue

--- javascript/flexslider.template.js
+++ javascript/flexslider.template.js
@@ -1,19 +1,21 @@
-$(document).ready(function() {
-    $('.flexslider_$sliderID').flexslider({
-        animation:      "$settings_animationType",           
-        easing:         "$settings_easing",                     // doesnt seem to make a difference
-        direction:      "$settings_direction",               
-        reverse:         $settings_reverse,                     
-        animationLoop:   $settings_animationLoop,         
-        slideshow:       $settings_doSlideshow,               
-        slideshowSpeed: "$settings_slideshowSpeed",     
-        animationSpeed: "$settings_animationSpeed",     
-        randomize:       $settings_randomize,                 
-        controlNav:      $settings_showControlNav            
-        //before: FLXslideBeforeHandler
+(function($) {
+    $(function () {
+        $('.flexslider_$sliderID').flexslider({
+            animation:      "$settings_animationType",           
+            easing:         "$settings_easing",                     // doesnt seem to make a difference
+            direction:      "$settings_direction",               
+            reverse:         $settings_reverse,                     
+            animationLoop:   $settings_animationLoop,         
+            slideshow:       $settings_doSlideshow,               
+            slideshowSpeed: "$settings_slideshowSpeed",     
+            animationSpeed: "$settings_animationSpeed",     
+            randomize:       $settings_randomize,                 
+            controlNav:      $settings_showControlNav            
+            //before: FLXslideBeforeHandler
+        });
     });
-    
-});
+})(jQuery);
+
 
 FLXslideBeforeHandler = function() {
     //console.log("before!");
